6fa552c8f3bc3b8878370cfb0e60aa867d5fbf04
Vinh To <misoul@gmail.com>
2016-07-26 16:25:00 -0700

    Make Sort generic so we can sort beyond numbers.

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-APPNAME=DigestLogfile
+APPNAME=Sort
 
 scalac -d target src/com/misoul/main/${APPNAME}.scala && \
-  scala -cp ./target com.misoul.main.$APPNAME 4 345 5634 534 453 543
+  scala -cp ./target com.misoul.main.$APPNAME 4 345 534 453 543 s as dsa s23 324s asd  34 ds
--- src/com/misoul/main/Sort.scala
+++ src/com/misoul/main/Sort.scala
@@ -1,12 +1,19 @@
 package com.misoul.main
 
 object Sort {
-  trait Sorter {
-    def sort(a: Seq[Integer]): Seq[Integer]
+  trait Sorter[T] {
+    def sort(a: Seq[T]): Seq[T]
   }
 
-  class QuickSort extends Sorter {
-    def sort(a: Seq[Integer]): Seq[Integer] = {
+  class BubbleSort[T <% Ordered[T]] extends Sorter[T] {
+    override def sort(a: Seq[T]): Seq[T] = {
+      // Do-able, but a bit silly
+      return Seq.empty
+    }
+  }
+
+  class QuickSort[T <% Ordered[T]] extends Sorter[T] {
+    def sort(a: Seq[T]): Seq[T] = {
       if (a.length < 2) a
       else {
         val pivot = a(0)
@@ -15,15 +22,15 @@ object Sort {
     }
   }
 
-  class MergeSort extends Sorter {
-    private def merge(a: Seq[Integer], b: Seq[Integer]): Seq[Integer] = {
+  class MergeSort[T <% Ordered[T]] extends Sorter[T] {
+    private def merge(a: Seq[T], b: Seq[T]): Seq[T] = {
       (a ++ b) sorted // CHEATING but less important
                       // TODO: improve, see `scalac -feature src/com/misoul/main/*`
     }
 
-    def sort(a: Seq[Integer]): Seq[Integer] = {
+    def sort(a: Seq[T]): Seq[T] = {
       a.size match {
-      case 0 => Seq.empty[Integer] // Should not happen
+      case 0 => Seq.empty[T] // Should not happen
       case 1 => a
       case 2 => merge(sort(a.slice(0, 1)), sort(a.slice(1, 2)))
       case _ =>
@@ -34,9 +41,9 @@ object Sort {
   }
 
   def main(args: Array[String]): Unit = {
-    val qSort = new QuickSort
-    val mSort = new MergeSort
-    val argsInt = args map(new Integer(_)) toSeq
+    val qSort = new QuickSort[String]
+    val mSort = new MergeSort[String]
+    val argsInt = args map(new String(_)) toSeq
 
     println("Inputs of length[" + args.length + "]: " + args.toSeq.mkString(","))
 
