81b32d5f4988686e4a053d7065426f87d15f5aa0
Vinh To <misoul@gmail.com>
2016-07-28 16:07:11 -0700

    Add example for consuming StdIn and simple processing

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-APPNAME=Sort
+APPNAME=ConsumeStdin
 
 scalac -d target src/com/misoul/main/${APPNAME}.scala && \
-  scala -cp ./target com.misoul.main.$APPNAME 4 345 534 453 543 s as dsa s23 324s asd  34 ds
+  scala -deprecation -cp ./target com.misoul.main.$APPNAME "$@"
